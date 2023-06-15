0f19ed359981de2638785b503fe4292f585d7f78
savindu29 <1savindupanagoda@gmail.com>
2023-06-15 21:39:40 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'',redirectTo:"/process/landing",pathMatch:"full"},
+  { path: 'process', loadChildren: () => import('./modules/process/process.module').then(m => m.ProcessModule) },
+  { path: 'share', loadChildren: () => import('./modules/share/share.module').then(m => m.ShareModule) },
+  { path: 'security', loadChildren: () => import('./modules/security/security.module').then(m => m.SecurityModule) },
+  { path: 'console', loadChildren: () => import('./modules/console/console.module').then(m => m.ConsoleModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
